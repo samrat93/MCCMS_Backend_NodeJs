569ceaa75f50b28239b1faea24254c67e88b7fb1
samrat <samrat.b.shah2016@gmail.com>
2022-04-27 17:37:40 +0530

    pagination in complaint list

--- src/controllers/complaintController.js
+++ src/controllers/complaintController.js
@@ -85,10 +85,26 @@ const ComplaintGet = async (req, res) => {
   }
 };
 
-const ListAllComplaint = async (req, res) => {
+const ListAllComplaint = async (req, res, next) => {
   try {
+    let { page, limit, sort } = req.query;
+    const size = parseInt(limit);
+    let start = 0;
+    if (!page) {
+      page = 1;
+    }
+    if (!limit) {
+      limit = 2;
+    }
+    if (page) {
+      start = (page - 1) * limit;
+    }
+
     const complaintList = await Complaint.find({});
-    res.status(200).send(complaintList);
+    const complist = complaintList.slice(start, start + Number(limit));
+    const total = Math.ceil(complaintList.length / Number(limit));
+
+    res.status(200).send({ page, limit, total, complist });
   } catch (e) {
     res.status(404).send(e.message);
   }
