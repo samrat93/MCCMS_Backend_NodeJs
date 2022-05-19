a8593ec04e9987317ecadc734ecde42bb9466f85
samrat bikram shah <samrat.b.shah2016@gmail.com>
2022-05-19 18:13:15 +0530

    change env

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://admin:7ZPblcIXRwWYDeF4@cluster0.98rdb.mongodb.net/mccmsDB",
+    // "mongodb+srv://admin:7ZPblcIXRwWYDeF4@cluster0.98rdb.mongodb.net/mccmsDB",
+    "mongodb+srv://admin:7ZPblcIXRwWYDeF4@cluster0.98rdb.mongodb.net/mccmsDB?retryWrites=true&w=majority"
     {
       useNewUrlParser: true,
     }
